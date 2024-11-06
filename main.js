e77a4d204bb93d32429e137c871af782b2216594
Vincent Mitchellgit config --global --listgit config --global user.name Vincent <Vincent.mitchell@stu.ocsb.ca>
2024-11-06 10:29:28 -0500

    11/06/2024

--- main.js
+++ main.js
@@ -27,3 +27,36 @@ function round(value, decimals) {
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+
+
+// Setup the menu
+let message = `Hi! Please make a selection:
+1 - Play
+2 - Options
+3 - DLC
+4 - Check for Updates
+5 - Exit`;
+
+console.log(message);
+
+const choice = prompt("Please select an option:\n1 - Play\n2 - Options\n3 - DLC\n4 - Check for Updates\n5 - Exit");
+
+if (choice === '1') {
+    console.log("Starting the game...");
+} else if (choice === '2') {
+    console.log("Opening options menu...");
+} else if (choice === '3') {
+    console.log("Accessing DLC...");
+} else if (choice === '4') {
+    console.log("Checking for updates...");
+} else if (choice === '5') {
+    console.log("Exiting the game...");
+} else {
+    console.log("Invalid choice. Please select a valid option.");
+}
+
+
